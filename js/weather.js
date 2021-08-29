5688dc864f79bae927250d32ddc80797f4fb6bca
kamruz-zaman8991 <70217992+kamruz-zaman8991@users.noreply.github.com>
2021-08-29 07:16:48 +0600

    default city weather added

--- js/weather.js
+++ js/weather.js
@@ -8,9 +8,7 @@ const loadWeather = () => {
 
     getWeather.value = '';
 }
-
 const setData = data => {
-    console.log(data);
     const getCurrentData = data.current;
     const location = data.location;
     const div = document.getElementById('div');
@@ -18,7 +16,26 @@ const setData = data => {
         <div class="result-1">
             <img width="50%" src="${getCurrentData.weather_icons[0]}">
             <h2>${location.name},${location.country}</h2>
-            <h2>${getCurrentData.temperature}</h2>
+            <h2>${getCurrentData.temperature} degree celcius</h2>
+            <h3>${getCurrentData.weather_descriptions[0]}</h3>
+        </div>`
+}
+const difault = () => {
+    url = `http://api.weatherstack.com/current?access_key=78cea249bb9983f27410b4b12d7af9d1&query=Dhaka`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => setDifault(data))
+}
+difault()
+const setDifault = (data) => {
+    const getCurrentData = data.current;
+    const location = data.location;
+    const div = document.getElementById('div');
+    div.innerHTML = `
+        <div class="result-1">
+            <img width="50%" src="${getCurrentData.weather_icons[0]}">
+            <h2>${location.name},${location.country}</h2>
+            <h2>${getCurrentData.temperature} degree celcius</h2>
             <h3>${getCurrentData.weather_descriptions[0]}</h3>
         </div>`
 }
